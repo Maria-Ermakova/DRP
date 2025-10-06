ea3de9e99bdc8de00122197eebd4a9d546403e52
Maria Ermakova <mariaermishina@yandex.ru>
2025-10-06 16:20:15 +0300

    Add all files

---
+++ backup_mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Mysql backup script
+
+#set -x
+#iterating through databases
+for s in mysql `mysql -N -e "SHOW DATABASES"`;
+  do
+  #creating a directory for each database found
+  mkdir $s;
+  #iterating through all the database tables
+  for table in `mysql -N -e "USE $s; SHOW TABLES FROM $s"`;
+    do
+    #The system version of the command creates a dump of the database table, compresses it (fast), and writes the file to a directory
+    /home/leo/mysqldump --add-drop-table --single-transaction --events --routines --triggers --source-data=2 --set-gtid-purged=COMMENTED \
+$s $table 2>&1 | grep -v "Warning" | gzip -1 > $s/$table.gz;
+    done
+  done
