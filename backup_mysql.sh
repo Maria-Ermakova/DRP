7a16024170eedc00482de5ae34189bdff320691a
Maria-Ermakova <mariaermishina87@gmail.com>
2025-10-14 11:24:59 +0300

    Update backup_mysql.sh

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -3,6 +3,8 @@
 #Mysql backup script
 
 #set -x
+mkdir -p /root/DRP/mysqldump/
+cd /root/DRP/mysqldump/
 #iterating through databases
 for s in mysql `mysql -N -e "SHOW DATABASES"`;
   do
@@ -12,7 +14,7 @@ for s in mysql `mysql -N -e "SHOW DATABASES"`;
   for table in `mysql -N -e "USE $s; SHOW TABLES FROM $s"`;
     do
     #The system version of the command creates a dump of the database table, compresses it (fast), and writes the file to a directory
-    /home/leo/mysqldump --add-drop-table --single-transaction --events --routines --triggers --source-data=2 --set-gtid-purged=COMMENTED \
+    mysqldump --add-drop-table --single-transaction --events --routines --triggers --source-data=2 --set-gtid-purged=COMMENTED \
 $s $table 2>&1 | grep -v "Warning" | gzip -1 > $s/$table.gz;
     done
   done
