6073671815ec86be02737167ddb9d13991a0e57c
Maria-Ermakova <mariaermishina87@gmail.com>
2025-10-17 16:13:33 +0300

    Update VM1_script.sh

--- VM1_script.sh
+++ VM1_script.sh
@@ -9,14 +9,14 @@ cp default /etc/nginx/sites-enabled/default
 service nginx restart
 
 #установка агента filebeat для сбора и пересылки логов с вебсервера nginx и замена конфига
-dpkg -i filebeat_8.17.1.deb
+#dpkg -i filebeat_8.17.1.deb
 cp filebeat.yml /etc/filebeat/filebeat.yml
 systemctl restart filebeat
 
 #установка и запуск системы мониторинга 
 apt install prometheus -y
 apt-get install -y adduser libfontconfig1 musl
-dpkg -i grafana_11.2.2.deb
+#dpkg -i grafana_11.2.2.deb
 systemctl daemon-reload
 systemctl enable --now prometheus
 systemctl enable --now grafana-server
